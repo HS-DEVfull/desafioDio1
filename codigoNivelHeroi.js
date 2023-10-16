14d1d3896a863a71f9b11284c17eb685b664dcb4
HS-DEVfull <148017848+HS-DEVfull@users.noreply.github.com>
2023-10-16 15:56:04 -0300

    Add files via upload

---
+++ codigoNivelHeroi.js
@@ -0,0 +1,24 @@
+let xp=10000
+let heroi="TecCheeta"
+
+if (xp<=1000){      
+ console.log("O Heroi está no nivel Ferro");
+}
+if (!(xp<1001) && (xp<=2000)){      
+    console.log("O Heroi " +heroi+ "está no nivel Bronze")   
+}
+if (!(xp<2001) && (xp<=6000)){      
+    console.log("O Heroi " +heroi+ "está no nivel Prata")
+}
+if (!(xp<6001) && (xp<=7000)){      
+    console.log("O Heroi " +heroi+ " está no nivel Ouro")
+}
+if (!(xp<7001) && (xp<=8000)){      
+    console.log("O Heroi " +heroi+ "está no nivel Platina")
+}
+if (!(xp<8001) && (xp<=9000)){      
+    console.log("O Heroi " +heroi+ " está no nivel Ascendente")
+}
+if (!(xp<9001) && (xp<=10000)){      
+    console.log("O Heroi " +heroi+ " está no nivel Imortal")
+}
